104f463a1795b97fe88bb7edd60d6463989e14da
RomanMillan <romandos962m@gmail.com>
2023-09-23 12:00:23 +0200

    respuesta servlet

--- src/main/java/RespuestaName.java
+++ src/main/java/RespuestaName.java
@@ -1,17 +1,37 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/RespuestaName")
 public class RespuestaName extends HttpServlet{
 
-	@Override
+	private static final long serialVersionUID = 1L;
+    
+
+    public RespuestaName() {
+        super();
+
+    }
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		String name = req.getParameter("nombre");
+		
+		PrintWriter writer= resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Hola " + name);
+		writer.println("</body>");
+		writer.println("</html>"); 
+		
 	}
 
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
 }
